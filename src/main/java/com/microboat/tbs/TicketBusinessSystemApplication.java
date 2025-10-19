6c28cd63fb807772f6c7949a631d5efc85f2e741
codeweiz <13955645241@163.com>
2025-10-19 22:06:30 +0800

    feat(init)：初始化项目

---
+++ src/main/java/com/microboat/tbs/TicketBusinessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.microboat.tbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBusinessSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBusinessSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microboat/tbs/TicketBusinessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microboat.tbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBusinessSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
